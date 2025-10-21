05a65c44db67b9513803101ac67e6a7e93485fb8
Alessandracancelier2501 <alessandra.cancelier@escola.pr.gov.br>
2025-10-21 09:24:44 -0300

    21/10

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", motraPalavrasChave);
+function motraPalavrasChave(){
+alert("Fui clicado!");
+
+}
