6263190825da210bfe85ac25655a2ec4d7b53a30
Alessandracancelier2501 <alessandra.cancelier@escola.pr.gov.br>
2025-10-28 10:17:25 -0300

    28/10

--- script.js
+++ script.js
@@ -8,19 +8,18 @@ function mostraPalavrasChave() {
 
 }
 
-function processaTexto(texto){
+function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
 
-let frequencias = {;
-for(let i of palavras)
-    frequencias[i]=0;
-for ( let j of palavras){
-    if (i  == j){
-        frequencias [i]++;
+    let frequencias = {}
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
     }
-}
-
-}
     return palavras;
 }
 
